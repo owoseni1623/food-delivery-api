03b528a04f49043ba3a903947e8da7d70b5e0dca
god_is_able <owoseni0816@gmail.com>
2024-08-18 07:27:19 -0700

    server

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,31 +1,3 @@
-// const User = require("../models/User");
-
-// const verifyEmail = async (req, res) => {
-//     const { token } = req.params;
-
-//     try {
-//         const user = await User.findOne({ verificationToken: token });
-
-//         if (!user) {
-//             return res.status(400).json({ success: false, message: 'Invalid verification token.' });
-//         }
-
-//         user.isVerified = true;
-//         user.verificationToken = undefined;
-//         user.verificationTokenExpires = undefined;
-
-//         await user.save();
-
-//         res.redirect(`${process.env.FRONTEND_URL}/login`);
-//     } catch (error) {
-//         res.status(500).json({ success: false, message: error.message });
-//     }
-// };
-
-// module.exports = { verifyEmail };
-
-
-
 const User = require("../models/User");
 
 const verifyEmail = async (req, res) => {
