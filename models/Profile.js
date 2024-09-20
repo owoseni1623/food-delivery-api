62d8185c7e924dfea7682890243047075384b128
god_is_able <owoseni0816@gmail.com>
2024-09-20 15:32:48 -0700

    image

--- models/Profile.js
+++ models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
+        ref: 'User',
         required: true,
         unique: true
     },
@@ -14,8 +14,36 @@ const profileSchema = new mongoose.Schema({
     address: String,
     image: {
         type: String,
+        default: null
     },
-}, { minimize: false });
+    imageUrl: {
+        type: String,
+        default: null
+    }
+}, { 
+    minimize: false,
+    timestamps: true 
+});
+
+// Pre-save hook to ensure imageUrl is always set when image is present
+profileSchema.pre('save', function(next) {
+    if (this.image) {
+        this.imageUrl = `/uploads/${this.image}`;
+    }
+    next();
+});
+
+// Virtual for full name
+profileSchema.virtual('fullName').get(function() {
+    return `${this.firstName} ${this.lastName}`;
+});
+
+// Method to get public profile
+profileSchema.methods.getPublicProfile = function() {
+    const publicProfile = this.toObject();
+    delete publicProfile.userId;
+    return publicProfile;
+};
 
 const Profile = mongoose.model("Profile", profileSchema);
 
