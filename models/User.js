b3de810bab8d5c9947df93b62443a89cd8f3708a
god_is_able <owoseni0816@gmail.com>
2024-09-26 14:51:02 -0700

    user

--- models/User.js
+++ models/User.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 
+const addressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    trim: true
+  },
+  city: {
+    type: String,
+    trim: true
+  },
+  state: {
+    type: String,
+    trim: true
+  },
+  country: {
+    type: String,
+    trim: true
+  }
+}, { _id: false });
+
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -24,10 +43,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  address: {
-    type: String,
-    trim: true
-  },
+  address: addressSchema,
   role: {
     type: String,
     default: "client"
